38a26bf5def3f1f4efeda479f0f1a900eb410638
Rúni H.Hansen <runi.hansen@okkara.net>
2023-06-06 17:29:14 +0200

    setup.sh is now checking if tmux, bc and sed are installed

--- setup.sh
+++ setup.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+TMUX=$(which tmux)
+SED=$(which sed)
+BC=$(which bc)
+if [[ "$TMUX" == "" ]]; then
+  echo "tmux is not installed!"
+  exit 1
+fi
+if [[ "$BC" == "" ]]; then
+  echo "bc is not installed!"
+  exit 2
+fi
+if [[ "$SED" == "" ]]; then
+  echo "sed is not installed!"
+  exit 3
+fi
 TPM_FOLDER=~/.config/tmux/plugins/tpm
 if [[ ! -d "$TPM_FOLDER" ]]; then
   git clone https://github.com/tmux-plugins/tpm $TPM_FOLDER
